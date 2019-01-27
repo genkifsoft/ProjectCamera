0e667c501afc9481894afdf04ce575192f01b853
unknown <genkifsoft@gmail.com>
2019-01-28 00:02:30 +0700

    Up code Project Name MVC

---
+++ admin/controller/News_Controller.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('PATH_SYSTEM')) die ('Bad requested!');
+ 
+class News_Controller extends FT_Controller
+{
+    public function indexAction()
+    {
+        echo '<pre>';
+        print_r($this);
+        echo '<h1>Index Action</h1>';
+    }
+}
